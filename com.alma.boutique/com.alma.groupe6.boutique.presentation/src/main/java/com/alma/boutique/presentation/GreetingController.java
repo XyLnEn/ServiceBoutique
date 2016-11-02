7f03c9a2f4f68edc6c5c48b734fc0b76cd838145
Xylnen <lenny.lucas44@gmail.com>
2016-11-02 16:03:19 +0100

    add spring for presentation layer

---
+++ com.alma.boutique/com.alma.groupe6.boutique.presentation/src/main/java/com/alma/boutique/presentation/GreetingController.java
@@ -0,0 +1,37 @@
+package com.alma.boutique.presentation;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+public class GreetingController {
+
+	public class Greeting {
+		
+	    private final long id;
+	    private final String content;
+
+	    public Greeting(long id, String content) {
+	        this.id = id;
+	        this.content = content;
+	    }
+
+	    public long getId() {
+	        return id;
+	    }
+
+	    public String getContent() {
+	        return content;
+	    }
+	}
+	
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+}
--- com.alma.boutique/com.alma.groupe6.boutique.presentation/src/main/java/com/alma/boutique/presentation/Presentation.java
+++ com.alma.boutique/com.alma.groupe6.boutique.presentation/src/main/java/com/alma/boutique/presentation/Presentation.java
@@ -1,7 +1,20 @@
 package com.alma.boutique.presentation;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
 /**
  * Created by thomas on 25/10/16.
  */
+@SpringBootApplication
 public class Presentation {
+	public static void main(String args[]) {
+		SpringApplication.run(Presentation.class);
+	}
 }
