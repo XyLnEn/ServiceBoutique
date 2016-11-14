0e550f9c702efe2b23299c660ce038f456584a94
Thomas Minier <tminier01@gmail.com>
2016-11-14 10:28:47 +0100

    fix bug with float comparaison

--- com.alma.boutique/com.alma.groupe6.boutique.domain/src/main/java/com/alma/boutique/domain/product/Price.java
+++ com.alma.boutique/com.alma.groupe6.boutique.domain/src/main/java/com/alma/boutique/domain/product/Price.java
@@ -43,7 +43,7 @@ public class Price {
 
     @Override
     public int hashCode() {
-        int result = (value != +0.0f ? Float.floatToIntBits(value) : 0);
+        int result = Float.compare(value, 0.0f) == 0 ? Float.floatToIntBits(value) : 0;
         result = 31 * result + currency.hashCode();
         return result;
     }
