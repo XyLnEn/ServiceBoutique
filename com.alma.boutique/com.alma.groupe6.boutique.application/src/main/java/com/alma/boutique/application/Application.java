6a2b220b6819dfe2f4af4af3d1b53706620e0b46
Thomas Minier <tminier01@gmail.com>
2016-11-18 11:14:08 +0100

    add canonical example with spark-java in application

--- com.alma.boutique/com.alma.groupe6.boutique.application/src/main/java/com/alma/boutique/application/Application.java
+++ com.alma.boutique/com.alma.groupe6.boutique.application/src/main/java/com/alma/boutique/application/Application.java
@@ -1,7 +1,19 @@
 package com.alma.boutique.application;
 
+import com.alma.boutique.application.controllers.GreetingController;
+
+import static spark.Spark.after;
+
 /**
- * Created by thomas on 25/10/16.
+ *
+ * @author Thomas Minier
  */
 public class Application {
+    public static void main(String[] args) {
+        // use a filter to convert all request into JSON
+        after((req, res) -> res.type("application/json"));
+
+        // create all the REST controller
+        GreetingController controller = new GreetingController();
+    }
 }
