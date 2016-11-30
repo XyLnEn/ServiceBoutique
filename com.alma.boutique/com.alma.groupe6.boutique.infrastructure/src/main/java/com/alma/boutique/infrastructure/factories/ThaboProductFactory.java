5a2d9845dede82f2d5b687c477fa7da3a9850102
Xylnen <lenny.lucas44@gmail.com>
2016-11-30 18:36:30 +0100

    Factory for ThaboProduct now buy the product

--- com.alma.boutique/com.alma.groupe6.boutique.infrastructure/src/main/java/com/alma/boutique/infrastructure/factories/ThaboProductFactory.java
+++ com.alma.boutique/com.alma.groupe6.boutique.infrastructure/src/main/java/com/alma/boutique/infrastructure/factories/ThaboProductFactory.java
@@ -3,9 +3,12 @@ package com.alma.boutique.infrastructure.factories;
 import com.alma.boutique.api.IFactory;
 import com.alma.boutique.domain.product.Price;
 import com.alma.boutique.infrastructure.conversion.ThaboProduct;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
 
 /**
  * @author Thomas Minier
@@ -21,19 +24,28 @@ public class ThaboProductFactory implements IFactory<ThaboProduct> {
   
 
 
-	public ThaboProductFactory(String id, String name, String price, String description, String productType, int quantity) {
+	public ThaboProductFactory(String id) {
+		
 		this.id = id;
-		this.name = name;
-		this.price = price;
-		this.description = description;
-		this.productType = productType;
-		this.quantity = quantity;
 	}
 
-
+  /**
+   * Private method used to create a HTTP connexion pre-configured via an API
+   * @param url the url of the webservice that we are trying to contact
+   * @return an HTTP connexion configured for the webservice
+   * @throws IOException
+   */
+  private HttpURLConnection setupConnection(String url) throws IOException {
+      URL service = new URL(url);
+      HttpURLConnection httpCon = (HttpURLConnection) service.openConnection();
+      httpCon.addRequestProperty("User-Agent", "Mozilla/4.76");
+      return httpCon;
+  }
 
 	@Override
   public ThaboProduct create() throws IOException {
-		return new ThaboProduct(id, name, price, description, productType, quantity);
+		ObjectMapper mapper = new ObjectMapper();
+		HttpURLConnection httpCon = setupConnection("https://fluffy-stock.herokuapp.com/api/product/" + id + "/order/1");
+		return mapper.readValue(httpCon.getInputStream(), ThaboProduct.class);
   }
 }
