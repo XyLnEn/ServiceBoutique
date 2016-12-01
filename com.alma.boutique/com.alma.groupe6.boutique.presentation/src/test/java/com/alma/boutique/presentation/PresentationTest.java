557f2467569811ff6ac5159cad1d95a41d58392c
Xylnen <lenny.lucas44@gmail.com>
2016-12-01 21:08:57 +0100

    refactoring for sonarqube, tests

--- com.alma.boutique/com.alma.groupe6.boutique.presentation/src/test/java/com/alma/boutique/presentation/PresentationTest.java
+++ com.alma.boutique/com.alma.groupe6.boutique.presentation/src/test/java/com/alma/boutique/presentation/PresentationTest.java
@@ -1,10 +1,17 @@
 package com.alma.boutique.presentation;
 
-import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 /**
  * Created by thomas on 25/10/16.
  */
 public class PresentationTest {
 
+	@Test
+	public void testInit() {
+		PresentationServlet p = new PresentationServlet();
+		p.init();
+		assert(true);
+	}
 }
